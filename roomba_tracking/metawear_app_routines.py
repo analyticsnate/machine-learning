588544697ded6eb59dd27900737c2a8faa157b09
analyticsnate <nmuth87@gmail.com>
2019-02-25 20:14:47 -0600

    initial commit for routines

---
+++ roomba_tracking/metawear_app_routines.py
@@ -0,0 +1,21 @@
+from metawear_app_core import Sensor
+
+def base_routine(name, seconds):
+    """
+    Connect to the Metawear sensor, and start collecting accelerometer data for 
+    a given number of seconds. Then save the data as a .csv in the data folder.
+
+    Params:
+    -------
+    name : string
+    a description of the routine you're gathering data for
+
+    seconds : int
+    the number of seconds to record the data
+    """
+    s = Sensor()
+    s.connect_to_sensor()
+    s.gather_data(seconds)
+    s.download_data(name)
+    s.reset_device()
+    print('Routine: ' + name + ' has completed.')
